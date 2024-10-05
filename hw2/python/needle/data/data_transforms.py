db4af8a3f20d115e286b9ff8aad82e0567a1f64c
abitha-thankaraj <abitha7thankaraj@gmail.com>
2024-10-05 18:53:23 -0400

    data augmentatations

--- hw2/python/needle/data/data_transforms.py
+++ hw2/python/needle/data/data_transforms.py
@@ -20,7 +20,9 @@ class RandomFlipHorizontal(Transform):
         """
         flip_img = np.random.rand() < self.p
         ### BEGIN YOUR SOLUTION
-        raise NotImplementedError()
+        if flip_img:
+            img = np.fliplr(img)
+        return img
         ### END YOUR SOLUTION
 
 
@@ -38,5 +40,21 @@ class RandomCrop(Transform):
         """
         shift_x, shift_y = np.random.randint(low=-self.padding, high=self.padding+1, size=2)
         ### BEGIN YOUR SOLUTION
-        raise NotImplementedError()
+        H, W, C = img.shape
+        
+        # Zero pad
+        padded_img = np.pad(img, ((self.padding, self.padding), (self.padding, self.padding), (0, 0)), mode='constant')
+        
+        # Calculate the starting points for cropping
+        start_x = self.padding + shift_x
+        start_y = self.padding + shift_y
+        
+        # Crop stays within the padded image bounds
+        start_x = max(0, min(start_x, 2*self.padding))
+        start_y = max(0, min(start_y, 2*self.padding))
+        
+        # Crop
+        cropped_img = padded_img[start_x:start_x+H, start_y:start_y+W, :]
+        
+        return cropped_img
         ### END YOUR SOLUTION
